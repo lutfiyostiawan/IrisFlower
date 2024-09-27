1a8d132e9a8cf63c04ddd61b4a5dc936eeb1d856
lutfiyostiawan <163811006+lutfiyostiawan@users.noreply.github.com>
2024-09-27 09:41:24 +0700

    Update iris.py

--- iris.py
+++ iris.py
@@ -9,6 +9,7 @@ with open('iris_model.pkl', 'rb') as f:
     model = pickle.load(f)
 
 species = ['setosa', 'versicolor', 'virginica']
+image = ['setosa.jpg', 'versicolor.jpg', 'virginica.jpg']
 
 st.title("Iris Flower Classification")
 st.write("This app correctly classifies iris flower among 3 possible species")
@@ -22,12 +23,19 @@ petal_width = st.sidebar.number_input("petal width (cm)", 0.1, 2.5, 0.2)
 
 # Button to trigger prediction
 if st.button("Predict"):
-# Getting Prediction from model
+    # Getting Prediction from model
     inp = np.array([sepal_length, sepal_width, petal_length, petal_width])
     inp = np.expand_dims(inp, axis=0)
     prediction = model.predict(inp)
 
-# Show Results when the button is clicked
+    # Show Results when the button is clicked
+    st.write('''
+    ## Results
+    Following is the probability of each class
+    ''')
+
+    df = pd.DataFrame(prediction, index=['result'], columns=species)
+    st.dataframe(df)
     result = species[np.argmax(prediction)]
     st.write("**This flower belongs to " + result + " class**")
     st.image(image[np.argmax(prediction)])
